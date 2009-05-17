5dd732c14c54478fb736ace619fd2e0987843c97
sylvain <sylvain@5b3156b2-dc0e-0410-86c6-8c750cf32934>
2009-05-17 21:18:53 +0000

    Bumping version to 0.2.0
    git-svn-id: https://svn.defuze.org/oss/headstock@820 5b3156b2-dc0e-0410-86c6-8c750cf32934

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
         
 setup(name = "headstock",
-      version = '0.1.0',
+      version = '0.2.0',
       description = "XMPP implementation using the Kamaelia library",
       maintainer = "Sylvain Hellegouarch",
       maintainer_email = "sh@defuze.org",
       url = "http://trac.defuze.org/wiki/headstock",
       download_url = "http://www.defuze.org/oss/headstock/",
       packages = ["headstock", "headstock.protocol",
-                  "headstock.protocol.extension",
+                  "headstock.protocol.extension", "headstock.client",
                   "headstock.protocol.core", "headstock.lib",
                   "headstock.lib.auth", "headstock.api"],
       platforms = ["any"],
       license = 'BSD',
       long_description = "XMPP implementation using the Kamaelia library",
-      install_requires= ['bridge>=0.3.5'],
+      install_requires= ['bridge>=0.3.6'],
      )
 
